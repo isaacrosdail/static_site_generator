7f1d886004cc16bf91cc85400d23200f8ac0336a
isaacrosdail <idrfritcher@gmail.com>
2026-06-11 18:15:39 -0500

    feat: add generate_page/s functions, & handler for copying static assets

--- src/main.py
+++ src/main.py
@@ -1,63 +1,84 @@
-
-import os
-
-from textnode import TextNode, TextType
-
-
-### SCRAPS from /tmp/ (to test)
 import os
 import shutil
+from pathlib import Path
 
-def list_contents_r(filepath: str | os.PathLike[str]) -> None:
-    filepath_str = str(filepath)
-    print("=================================")
-    print(f"now in: {filepath_str}")
-    for file in os.listdir(filepath):
-        filename: str = os.fsdecode(file)
-        print(f"{filepath_str}/{filename}")
-        full_path = os.path.join(filepath_str, filename)
-        if not os.path.isfile(full_path):
-            # get one layer deeper for next path
-            list_contents_r(full_path)
-    print("=================================")
+from block_markdown import markdown_to_html_node, extract_title
+
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    # from_path md file:
+    md_contents = ""
+    with open(from_path) as f:
+        md_contents = f.read()
+    template_contents = ""
+    with open(template_path) as f:
+        template_contents = f.read()
+
+    # Convert md_contents to an HTML string
+    md_as_html = markdown_to_html_node(md_contents).to_html()
+    # Use extract title to grab the title for page
+    page_title = extract_title(md_contents)
+    # Replace the {{ Title }} and {{ Content }} placeholders in the template with the HTML + title
+    replaced_template = template_contents.replace("{{ Title }}", page_title).replace("{{ Content }}", md_as_html)
+
+    # Write the new full HTML page to a file at dest_path
+    dest_dir_path = os.path.dirname(dest_path)
+    os.makedirs(dest_dir_path, exist_ok=True)
+    with open(dest_path, 'w') as f:
+        f.write(replaced_template)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    # Crawl every entry in content directory
+    # Recursive case? go thru dirs
+    for name in os.listdir(dir_path_content):
+        dir_path_content_child = os.path.join(dir_path_content, name)
+        dest_dir_path_child = os.path.join(dest_dir_path, name)
+        # Recursive case:
+        if os.path.isdir(dir_path_content_child):
+            generate_pages_recursive(dir_path_content_child, template_path, dest_dir_path_child)
+        # For each md file found, generate a new html file using the same template.html
+        # Base case? "stop" and call generate_page instead of generate_page_r?
+        if os.path.isfile(dir_path_content_child):
+            final_dest_path = Path(dest_dir_path_child).with_suffix(".html")
+            generate_page(dir_path_content_child, template_path, final_dest_path)
+
+
+def rebuild_public():
+    # Delete contents of public dir & recreate fresh
+    shutil.rmtree("public", ignore_errors=True) # so first run doesn't crash
+    os.mkdir("public")
+
+def copy_r(src: str | os.PathLike[str], dst: str | os.PathLike[str]) -> None:
+    for name in os.listdir(src):
+        src_child = os.path.join(src, name)
+        dst_child = os.path.join(dst, name)
+        # For dir -> ensure we mirror in dst, then recurse
+        # Recursive case
+        if os.path.isdir(src_child):
+            os.mkdir(dst_child)
+            copy_r(src_child, dst_child)
+        # For file -> copy
+        # Base case
+        if os.path.isfile(src_child):
+            shutil.copy(src_child, dst_child)
+
 
 
 def main():
-    public_dir_str = "public"
-    # 1. Wipe public dir
-    dir = os.fsencode(public_dir_str)
-    shutil.rmtree(dir)
-    # 2. Recreate public dir
-    os.mkdir(dir)
-    # 3. Recursively copy contents from static/ to public/
-    # list contents of static recursively?
-    static_dir_str = "static"
-    static_dir = os.fsencode(static_dir_str)
-    abs_path = os.path.abspath(".")
-    print(f"abs path here is: {os.path.abspath(".")}")
-    static_dir = os.path.normpath(os.path.join(abs_path, static_dir_str))
-    list_contents_r(static_dir)
 
+    # Rebuild public dir from scratch
+    rebuild_public()
+    # Recursively copy contents of static -> public
+    copy_r("static", "public")
 
-if __name__ == "__main__":
-    main()
-# =============================================================================
+    # Generate a page from content/index.md using template.html and write it to public/index.html
+    #####generate_page("content/index.md", "template.html", "public/index.html")
+    # Generate ALL our pages from our .md files:
+    generate_pages_recursive("content", "template.html", "public")
 
-
-
-
-
-
-def copier():
-    # Delete contents of public dir
-    dir = os.fsencode("public/")
-    for file in os.listdir(dir):
-
-
-def main():
-    print("hello world")
-    text_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(text_node)
 
 if __name__ == "__main__":
     main()
